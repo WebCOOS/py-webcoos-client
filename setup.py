3498ade965bfcf17b56fd60940621348552271e0
Matthew Conlin <conlin@jupyter.co-ops.nos.noaa.gov>
2024-10-21 20:20:09 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pywebcoos',
+    version='0.1.0',
+    description='Python wrapper for the WebCOOS API.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['numpy','pandas','pytz','requests']
+)
