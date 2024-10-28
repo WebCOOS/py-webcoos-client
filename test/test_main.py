3fc0ca8ff7b31321e792388f8dbe0619b159480c
conlin-matt <matthew.conlin@noaa.gov>
2024-10-28 15:20:17 +0000

    Add unit tests

--- test/test_main.py
+++ test/test_main.py
@@ -2,7 +2,7 @@ import os
 import pytest
 import pywebcoos
 
-
+# Input checking / error handling tests #
 def test_invalid_api_token_raises_exception():    
     with pytest.raises(ValueError, match="API access token is not valid."):
         api = pywebcoos.API(token='Invalid token')
@@ -41,9 +41,26 @@ def test_date_out_of_range_raises_exception():
     with pytest.raises(ValueError, match="At least one requested date bound is outside the range of available data for this product at this camera."):
         api.download('Charleston Harbor, SC','video-archive','190001011000','190001011010',1,'.')  
         
-
-
-def test_video_download():
+# Unit tests #
+def test_get_cameras():
+    key = os.getenv('API_KEY')
+    api = pywebcoos.API(str(key))
+    cams = api.get_cameras()
+    assert len(cams)>0 , 'Getting camera list failed'
+    
+def test_get_products():
+    key = os.getenv('API_KEY')
+    api = pywebcoos.API(str(key))
+    prods = api.get_products('Charleston Harbor, SC')
+    assert len(prods)>0 , 'Getting product list failed'    
+    
+def test_get_inventory():
+    key = os.getenv('API_KEY')
+    api = pywebcoos.API(str(key))
+    inv = api.get_inventory('Charleston Harbor, SC','video-archive')
+    assert len(inv)>0 , 'Getting product inventory failed' 
+    
+def test_download_videos():
     key = os.getenv('API_KEY')
     api = pywebcoos.API(str(key))
     fname = api.download('Charleston Harbor, SC',
@@ -55,3 +72,14 @@ def test_video_download():
     assert len(fname)>0 , 'Video download failed.'
     os.remove('nwlon_charleston-2024-01-01-150123Z.mp4')
 
+def test_download_images():
+    key = os.getenv('API_KEY')
+    api = pywebcoos.API(str(key))
+    fname = api.download('Charleston Harbor, SC',
+                         'one-minute-stills',
+                         '202401011000',
+                         '202401011001',
+                         1,
+                         '.')
+    assert len(fname)>0 , 'Image download failed.'
+    os.remove('nwlon_charleston-2024-01-01-150053Z.jpg')
